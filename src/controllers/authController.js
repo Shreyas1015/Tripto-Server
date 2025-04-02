df071d452a3a480adf9397cbe7160aa98c14821b
Shreyas <shreyas1234gurav@gmail.com>
2025-04-02 14:58:07 +0530

    Admin Drivers Details

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -140,13 +140,13 @@ const refresh = asyncHand((req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "None",
+     secure: true,
+     sameSite: "None",
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: true,
-    sameSite: "None",
+     secure: true,
+     sameSite: "None",
   });
   res.sendStatus(200);
 });
